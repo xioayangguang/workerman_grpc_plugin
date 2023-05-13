c881cf173b41569e57e4b6c94540ddd397c4892b
小阳光 <930029203@qq.com>
2023-05-13 23:25:21 +0800

    Update template.go

--- php/template.go
+++ php/template.go
@@ -3,34 +3,94 @@ package php
 import (
 	"bytes"
 	"fmt"
-	"strings"
-	"text/template"
-
 	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
+	"strings"
+	"text/template"
 )
 
 const phpBody = `<?php
-# source: {{ .File.Name }}
+declare(strict_types=1);
+# source: {{ .File.Name }}  
 {{ $ns := .Namespace -}}
 {{if $ns.Namespace}}
 namespace {{ $ns.Namespace }};
 {{end}}
+use parse\Http2Stream;
+use parse\Response;
 
 {{- range $n := $ns.Import}}
 use {{ $n }};
 {{- end}}
 
-interface {{ .Service.Name | interface }} extends Grpc\ServiceInterface
+
+class {{ .Service.Name | service }}
 {
-    public const NAME = "{{ .File.Package }}.{{ .Service.Name }}";{{ "\n" }}
+	 public static $Streaming = [
+	{{- range $k,$v := .Ttype}}
+		"{{$k}}"=>[{{- range $kk,$vv := $v }}"{{ $vv }}",{{- end}}],
+	{{- end}}
+	];
+	
+	{{ $s := .Service -}}
+	{{ $f := .File -}}
+	 public static $Route  = [
+	{{- range $m := .Service.Method}}
+		"/{{ $f.Package }}.{{ $s.Name }}/{{ $m.Name }}" => [{{ $s.Name }}Service::class, "{{ $m.Name }}"],
+	{{- end}}
+	];
+
+     public static $Parameter  = [
+	{{- range $m := .Service.Method}}
+		"/{{ $f.Package }}.{{ $s.Name }}/{{ $m.Name }}" => {{ name $ns $m.InputType }}::class,
+	{{- end}}
+	];
+
+    public const NAME = "{{ .File.Package }}.{{ .Service.Name }}";
+
 {{- range $m := .Service.Method}}
-    /**
-    * @param Context $context
-    * @param {{ name $ns $m.InputType }} $request
-    * @return {{ name $ns $m.OutputType }}
-    */
-    public function {{ $m.Name }}(Context $context, {{ name $ns $m.InputType }} $request): {{ name $ns $m.OutputType }};
+	{{if $m.ClientStreaming }}
+		{{if $m.ServerStreaming }}
+	/**
+	*此处实现自己的业务逻辑
+	* DoubleStreaming
+	* @param {{ name $ns $m.InputType }} $request
+	*/
+	public static function {{ $m.Name }}({{ name $ns $m.InputType }} $request) 
+    {
+	}
+		{{else}}
+	/**
+	*此处实现自己的业务逻辑
+	* ClientStreaming
+	* @param {{ name $ns $m.InputType }} $request
+	*/
+	public static function {{ $m.Name }}({{ name $ns $m.InputType }} $request) : {{ name $ns $m.OutputType }}
+    {
+	}
+		{{end}}
+	{{else}}
+		{{if $m.ServerStreaming }}
+	/**
+	*此处实现自己的业务逻辑
+	* ServerStreaming
+	* @param {{ name $ns $m.InputType }} $request
+	*/
+	public static function {{ $m.Name }}({{ name $ns $m.InputType }} $request) 
+    {
+	}
+		{{else}}
+	/**
+	*此处实现自己的业务逻辑
+	* Simple
+	* @param {{ name $ns $m.InputType }} $request
+	* @return {{ name $ns $m.OutputType }}
+	*/
+	public static function {{ $m.Name }}({{ name $ns $m.InputType }} $request): {{ name $ns $m.OutputType }} 
+    {
+	}
+		{{end}}
+{{end}}
 {{end -}}
 }
 `
@@ -41,8 +101,8 @@ var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.New("phpBody").Funcs(template.FuncMap{
-		"interface": func(name *string) string {
-			return identifier(*name, "interface")
+		"service": func(name *string) string {
+			return identifier(*name, "Service")
 		},
 		"name": func(ns *ns, name *string) string {
 			return ns.resolve(name)
@@ -56,7 +116,7 @@ func filename(file *descriptor.FileDescriptorProto, name *string) string {
 	if file.Options != nil && file.Options.PhpNamespace != nil {
 		ns = strings.Replace(*file.Options.PhpNamespace, `\`, `/`, -1)
 	}
-	return fmt.Sprintf("%s/%s.php", ns, identifier(*name, "interface"))
+	return fmt.Sprintf("%s/%s.php", ns, identifier(*name, "Service"))
 }
 
 // generate php file body
@@ -66,17 +126,36 @@ func body(
 	service *descriptor.ServiceDescriptorProto,
 ) string {
 	out := bytes.NewBuffer(nil)
-
+	ttype := make(map[string][]string)
+	for _, m := range service.Method {
+		path := fmt.Sprintf("/%s.%s/%s", *file.Package, *service.Name, *m.Name)
+		if m.ServerStreaming != nil && m.ClientStreaming != nil {
+			if *m.ServerStreaming && *m.ClientStreaming {
+				ttype["double_streaming"] = append(ttype["double_streaming"], path)
+				continue
+			}
+		}
+		if m.ServerStreaming != nil && *m.ServerStreaming {
+			ttype["server_streaming"] = append(ttype["server_streaming"], path)
+			continue
+		}
+		if m.ClientStreaming != nil && *m.ClientStreaming {
+			ttype["client_streaming"] = append(ttype["client_streaming"], path)
+			continue
+		}
+		ttype["simple"] = append(ttype["simple"], path)
+	}
 	data := struct {
 		Namespace *ns
 		File      *descriptor.FileDescriptorProto
 		Service   *descriptor.ServiceDescriptorProto
+		Ttype     map[string][]string
 	}{
 		Namespace: newNamespace(req, file, service),
 		File:      file,
 		Service:   service,
+		Ttype:     ttype,
 	}
-
 	err := tpl.Execute(out, data)
 	if err != nil {
 		panic(err)
